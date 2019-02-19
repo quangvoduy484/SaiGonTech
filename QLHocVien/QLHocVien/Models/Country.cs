7e78dcb6e9df2fc124c9290026d8a2a41611f1d6
YENVY <vovy1409@gmail.com>
2019-02-19 21:12:54 +0700

    update models: major, country, province

---
+++ QLHocVien/QLHocVien/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLHocVien.Models
+{
+    public class Country
+    {
+        public int CountryID { get; set; }
+        public string CountryName { get; set; }
+    }
+}
---
+++ QLHocVien/QLHocVien/Models/Province.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLHocVien.Models
+{
+    public class Province
+    {
+        public int ProvinceID { get; set; }
+        public string ProvinceName { get; set; }
+        public int CountryID { get; set; }
+
+        [ForeignKey("CountryID")]
+        public Country Country { get; set; }
+    }
+}

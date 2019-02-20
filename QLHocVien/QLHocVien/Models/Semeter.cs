897cac9c6b000d4d9024492cddc5cd5bc2ff7516
quangvoduy484 <quangvoduy484@gmail.com>
2019-02-20 14:49:33 +0700

    hoan thanh api semeter

---
+++ QLHocVien/QLHocVien/Models/Semeter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLHocVien.Models
+{
+    [Table("SEMESTER")]
+    public class Semeter
+    {
+        [Column("SEM_ID")]
+        public int Id { get; set; }
+
+        [Column("SEMESTERNAME")]
+        public string semeter_name { get; set; }
+    }
+}

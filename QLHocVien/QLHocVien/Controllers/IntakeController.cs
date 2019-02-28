2bb0cf65172ddb50d23dc4892f4dda009282649c
oriakita <35460456+oriakita@users.noreply.github.com>
2019-02-28 16:46:50 +0700

    add year and intake controller to master

---
+++ QLHocVien/QLHocVien/Controllers/IntakeController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QLHocVien.Models;
+using QLHocVien.Repones;
+using QLHocVien.Requests;
+
+namespace QLHocVien.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IntakeController : ControllerBase
+    {
+        private readonly QLHocVienContext _context;
+
+        public IntakeController(QLHocVienContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Baserepone>> Get()
+        {
+            var intakes = await _context.Intakes.ToListAsync();
+            return new Baserepone(intakes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Baserepone>> Get(int id)
+        {
+            var intakeItem = await _context.Intakes.FindAsync(id);
+            if (intakeItem == null)
+            {
+                return new Baserepone
+                {
+                    errorcode = 1,
+                    errormessage = "Intake Not Found",
+                };
+            }
+            else
+            {
+                return new Baserepone(intakeItem);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Baserepone>> Post(Intake intakeInput)
+        {
+            _context.Intakes.Add(intakeInput);
+            await _context.SaveChangesAsync();
+
+            return new Baserepone(intakeInput);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Baserepone>> Put(int id, Intake intakeInput)
+        {
+            var intake = await _context.Intakes.FindAsync(id);
+            if (intake == null)
+            {
+                return new Baserepone
+                {
+                    errorcode = 1,
+                    errormessage = "Not Found Intake Id: " + id
+                };
+            }
+            else
+            {
+                intake.IntakeName = intakeInput.IntakeName;
+                _context.Intakes.Update(intake);
+                await _context.SaveChangesAsync();
+                return new Baserepone(intake);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Baserepone>> Delete(int id)
+        {
+            var intake = await _context.Intakes.FindAsync(id);
+            if (intake == null)
+            {
+                return new Baserepone
+                {
+                    errorcode = 1,
+                    errormessage = "Not Found Intake Id: " + id
+                };
+            }
+            else
+            {
+                _context.Intakes.Remove(intake);
+                await _context.SaveChangesAsync();
+                return new Baserepone
+                {
+                    errorcode = 0,
+                    errormessage = "Delete Success: " + intake.IntakeName
+                };
+            }
+        }
+    }
+}
---
+++ QLHocVien/QLHocVien/Controllers/YearController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QLHocVien.Models;
+using QLHocVien.Repones;
+
+namespace QLHocVien.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YearController : ControllerBase
+    {
+        private readonly QLHocVienContext _context;
+
+        public YearController(QLHocVienContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Baserepone>> Get()
+        {
+            var years = await _context.Years.ToListAsync();
+            return new Baserepone(years);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Baserepone>> Get(int id)
+        {
+            var yearItem = await _context.Years.FindAsync(id);
+            if (yearItem == null)
+            {
+                return new Baserepone
+                {
+                    errorcode = 1,
+                    errormessage = "Year Not Found",
+                };
+            }
+            else
+            {
+                return new Baserepone(yearItem);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Baserepone>> Post(Year yearInput)
+        {
+            _context.Years.Add(yearInput);
+            await _context.SaveChangesAsync();
+
+            return new Baserepone(yearInput);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Baserepone>> Put(int id, Year yearInput)
+        {
+            var yearItem = await _context.Years.FindAsync(id);
+            if (yearItem == null)
+            {
+                return new Baserepone
+                {
+                    errorcode = 1,
+                    errormessage = "Not Found Year Id: " + id
+                };
+            }
+            else
+            {
+                yearItem.YearName = yearInput.YearName;
+                _context.Years.Update(yearItem);
+                await _context.SaveChangesAsync();
+                return new Baserepone(yearItem);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Baserepone>> Delete(int id)
+        {
+            var yearItem = await _context.Years.FindAsync(id);
+            if (yearItem == null)
+            {
+                return new Baserepone
+                {
+                    errorcode = 1,
+                    errormessage = "Not Found Year Id: " + id
+                };
+            }
+            else
+            {
+                _context.Years.Remove(yearItem);
+                await _context.SaveChangesAsync();
+                return new Baserepone
+                {
+                    errorcode = 0,
+                    errormessage = "Delete Success: " + yearItem.YearName
+                };
+            }
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<Year>>> Search([FromQuery] int yearinput)
+        {
+            return await _context.Years
+                .Where(x => x.YearName == yearinput)
+                .ToListAsync();
+        }
+    }
+}
